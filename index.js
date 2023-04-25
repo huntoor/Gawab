f066d765440a35b3beee84b672e0e66910a0e16b
huntoor <omarahmed99929@gmail.com>
2023-04-25 06:12:30 +0200

    First Commit

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const bodyParser = require('body-parser');
+const session = require('express-session');
+require('dotenv').config()
+
+// const cookieSession = require('cookie-session');
+
+
+const app = express();
+
+app.use(session({
+	secret: 'secret',
+  resave: false,
+	saveUninitialized: false
+}));
+app.use(express.json());
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+
+// const db = mysql.createConnection({
+//   host: 'localhost',
+//   user: 'hunter',
+//   password: 'password',
+//   database: 'test'
+// });
+
+// app.get('/', (req, res) => {
+//   res.sendFile(path.join(__dirname + '/views/signUp.html'));
+// });
+
+// db.connect(function (err) {
+//   if (err) {
+//     console.log('Error connecting to Database', err);
+//     return;
+//   }
+//   console.log('Connection established');
+// });
+
+// app.get('/users', (req, res) => {
+//   const sql = 'SELECT * FROM tableName';
+
+//   db.query(sql, (err, result) => {
+//     if (err) throw err;
+//     res.send(result);
+//   });
+// });
+
+// app.listen(process.env.PORT, () => console.log("Server Up"));
+app.listen(3000, () => console.log("Server Up"));
